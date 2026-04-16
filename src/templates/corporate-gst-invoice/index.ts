2db30ba835168f7f337e1e37af5fe4c227bdde20
Purvi-Gupta <107806285+Purvi-Gupta@users.noreply.github.com>
2026-04-16 14:08:24 +0530

    feat: add corporate-gst-invoice template

---
+++ src/templates/corporate-gst-invoice/index.ts
@@ -0,0 +1,13 @@
+// @ts-ignore - compiled via handlebars-loader
+import template from "./template.hbs";
+import { normalizeInvoiceTemplateState } from "../../main/invoiceTemplateNormalization";
+import "./styles.css";
+// Register widgets
+import "../../widgets/invoice-status";
+import "../../widgets/demo-badge";
+import "../../widgets/date-time";
+import "../../widgets/markdown-viewer";
+
+// Export template to global for main renderer to consume
+window.CeresTemplateDataMapper = normalizeInvoiceTemplateState as any;
+window.CeresTemplate = template;
