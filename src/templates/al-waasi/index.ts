6182628cdce6679e58833a87088442ee7e4d5e00
Purvi-Gupta <107806285+Purvi-Gupta@users.noreply.github.com>
2026-05-04 13:55:38 +0530

    feat: add al-waasi invoice template and update corporate-gst-invoice styles

---
+++ src/templates/al-waasi/index.ts
@@ -0,0 +1,19 @@
+// @ts-ignore - compiled via handlebars-loader
+import template from "./template.hbs";
+import { normalizeInvoiceTemplateState } from "../../main/invoiceTemplateNormalization";
+import "./styles.css";
+// Register widgets
+import "../../widgets/invoice-status";
+import "../../widgets/demo-badge";
+import "../../widgets/date-time";
+import "../../widgets/markdown-viewer";
+
+// Register custom helpers
+declare const Handlebars: any;
+Handlebars.registerHelper("increment", function (value: number) {
+  return value + 1;
+});
+
+// Export template to global for main renderer to consume
+window.CeresTemplateDataMapper = normalizeInvoiceTemplateState as any;
+window.CeresTemplate = template;
